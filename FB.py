f4601750ee2ae85ab8c4fad6f9f9fd16ef948939
latifahnh <skyver.hans@gmail.com>
2022-03-30 12:42:56 +0700

    Update FB.py

--- FB.py
+++ FB.py
@@ -20,13 +20,13 @@ os.path.exists('/home/tsel-ai/deploy/api/public/crawl/socmed')
 options = Options()
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu') 
+options.add_argument('--disable-gpu')
 options.add_argument("--disable-notifications")
-options.add_argument('--disable-setuid-sandbox') 
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
+options.add_argument('--disable-setuid-sandbox')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96                              .0.4664.110 Safari/537.36")
 
-#driver = webdriver.Chrome('/home/tsel-ai/bin/chromedriver', chrome_options=options)
-driver = webdriver.Chrome('chromedriver_win32/chromedriver.exe', chrome_options=options)
+driver = webdriver.Chrome('/home/lat/chromedriver', chrome_options=options)
+#driver = webdriver.Chrome('chromedriver_win32/chromedriver.exe', chrome_options=options)
 time.sleep(5)
 driver.get("http:/www.facebook.com")
 time.sleep(5)
@@ -34,9 +34,9 @@ time.sleep(5)
 username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
 password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
 username.clear()
-username.send_keys("diisi_sendiri")
+username.send_keys("isi sendiri")
 password.clear()
-password.send_keys("diisi_sendiri")
+password.send_keys("isi sendiri")
 
 try:
     button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
@@ -50,7 +50,7 @@ driver.get("http:/www.facebook.com")
 time.sleep(5)
 
 listData        =   []
-listAllMedia    =   [] 
+listAllMedia    =   []
 ListPost        =   []
 listThePost     =   []
 driver.execute_script("window.scrollTo(0, window.scrollY + 400)")
@@ -63,10 +63,10 @@ if stsLogin == 1:
     while(match==False):
         time.sleep(2)
         main_window     =  driver.current_window_handle
-        
+
         try:
-            lastFeed    = driver.find_element_by_xpath('//html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[3]/div/div[4]/div/div/div/div/div/div[3]/a')
-            
+            lastFeed    = driver.find_element_by_xpath('//html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/                              div[2]/div/div/div/div[3]/div/div[4]/div/div/div/div/div/div[3]/a')
+
         except Exception as Error:
             # print(Error)
             lastFeed    = ''
@@ -81,26 +81,26 @@ if stsLogin == 1:
                 listUrlMedia = []
                 listImage = []
                 thePosts  = []
-                scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);" + "var elementTop = arguments[0].getBoundingClientRect().top;" + "window.scrollBy(0, elementTop-(viewPortHeight/2));"
-                
-                akunNames = i.find_element_by_xpath("./div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[1]/span/h4/span[1]/a/strong/span")
+                scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.inne                              rHeight || 0);" + "var elementTop = arguments[0].getBoundingClientRect().top;" + "window.scrollBy(0, elementTop-(viewPortHe                              ight/2));"
+
+                akunNames = i.find_element_by_xpath("./div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[1]/span/h4/                              span[1]/a/strong/span")
                 driver.execute_script(scrollElementIntoMiddle, akunNames)
                 akunName  = akunNames.text.replace('\n','')
                 actions = ActionChains(driver)
                 actions.move_to_element(akunNames)
                 time.sleep(5)
-                datePost = i.find_element_by_xpath('./div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/span/span/span[2]/span/a')
+                datePost = i.find_element_by_xpath('./div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/span/span                              /span[2]/span/a')
                 actions.move_to_element(datePost).perform()
                 time.sleep(5)
-                datePost = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[2]/div/div/div[1]/span/div/div/span').text
+                datePost = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[2]/div/div/div                              [1]/span/div/div/span').text
                 joinAkunName = '_'.join(akunName.split())
                 joinDatePost = '_'.join(datePost.split())
                 idPost = '{}_{}'.format(joinAkunName, joinDatePost)
                 time.sleep(5)
                 urlImg      = i.find_elements_by_xpath("./div/div/div/div/div/div/div/div[3]/div[2]/div/div/*/*/*/*/div")
-                multipleImg = i.find_elements_by_xpath('./div/div/div/div/div/div/div/div[3]/div[2]/div/div/*/*/*/div/a/div/div/div') 
-                isMore   = True 
-        
+                multipleImg = i.find_elements_by_xpath('./div/div/div/div/div/div/div/div[3]/div[2]/div/div/*/*/*/div/a/div                              /div/div')
+                isMore   = True
+
                 while isMore:
 
                     try:
@@ -111,17 +111,17 @@ if stsLogin == 1:
                             i.find_element_by_xpath('//div[contains(text(), "Lihat Selengkapnya")]').click()
                             isMore = False
                             time.sleep(5)
-                        
+
                         else:
                             isMore = False
 
                     except Exception as Error:
                         # print(Error)
                         isMore = False
-                    
+
                 try:
-                    urlImgAdd   = i.find_elements_by_xpath("./div/div/div/div/div/div/div/div[3]/div/div/div/div/div/div/div/div/div/div")
-                
+                    urlImgAdd   = i.find_elements_by_xpath("./div/div/div/div/div/div/div/div[3]/div/div/div/div/div/div/di                              v/div/div/div")
+
                 except Exception as Error:
                     # print(Error)
                     urlImgAdd = []
@@ -129,33 +129,6 @@ if stsLogin == 1:
                 for media in urlImg:
 
                     try:
-                        time.sleep(2)    
-                        urls = media.find_element_by_tag_name('img').get_attribute('src')
-                        now = datetime.now()
-                        currentTime = now.strftime("%H_%M_%S")
-                        akunNameSplit = '_'.join(akunName)
-
-                        now = datetime.now()
-                        currentTime = now.strftime("%H_%M_%S")
-                        akunNameSplit = '_'.join(akunName)
-
-                        response = requests.get(urls, stream=True)
-                        time.sleep(2)    
-
-                        #filename = f"Facebook/{akunName}_{currentTime}.jpg"
-                        filename = f"images/{akunName}_{currentTime}.jpg".replace(' ','_')
-
-                        listImage.append({"image_file":response.content,'filename':filename})
-
-
-                    except Exception as Error:
-                        #print(Error)
-                        pass
-                
-
-                for media in multipleImg:
-
-                    try:    
                         time.sleep(2)
                         urls = media.find_element_by_tag_name('img').get_attribute('src')
                         now = datetime.now()
@@ -167,7 +140,34 @@ if stsLogin == 1:
                         akunNameSplit = '_'.join(akunName)
 
                         response = requests.get(urls, stream=True)
-                        time.sleep(2)    
+                        time.sleep(2)
+
+                        #filename = f"Facebook/{akunName}_{currentTime}.jpg"
+                        filename = f"images/{akunName}_{currentTime}.jpg".replace(' ','_')
+
+                        listImage.append({"image_file":response.content,'filename':filename})
+
+
+                    except Exception as Error:
+                        #print(Error)
+                        pass
+
+
+                for media in multipleImg:
+
+                    try:
+                        time.sleep(2)
+                        urls = media.find_element_by_tag_name('img').get_attribute('src')
+                        now = datetime.now()
+                        currentTime = now.strftime("%H_%M_%S")
+                        akunNameSplit = '_'.join(akunName)
+
+                        now = datetime.now()
+                        currentTime = now.strftime("%H_%M_%S")
+                        akunNameSplit = '_'.join(akunName)
+
+                        response = requests.get(urls, stream=True)
+                        time.sleep(2)
 
                         filename = f"images/{akunName}_{currentTime}.jpg".replace(' ','_')
 
@@ -191,20 +191,20 @@ if stsLogin == 1:
                         akunNameSplit = '_'.join(akunName)
 
                         response = requests.get(urls, stream=True)
-                        time.sleep(2)    
+                        time.sleep(2)
 
                         filename = f"images/{akunName}_{currentTime}.jpg".replace(' ','_')
-                        
+
                         listImage.append({"image_file":response.content,'filename':filename})
 
                     except Exception as Error:
                         # print(Error)
                         break
 
-                thePost   = i.find_elements_by_xpath("./div/div/div/div/div/div/div/div[3]/div[1]/div/div/div/span/div/div")
+                thePost   = i.find_elements_by_xpath("./div/div/div/div/div/div/div/div[3]/div[1]/div/div/div/span/div/div"                              )
 
                 for posts in thePost:
-                    
+
                     try:
                         if posts.text.replace("\n"," "):
                             thePosts.append(posts.text.replace("\n"," "))
@@ -212,11 +212,11 @@ if stsLogin == 1:
                     except Exception as Error:
                         # print(Error)
                         pass
-                    
-    
+
+
                     try:
                         theEmoji = posts.find_elements_by_xpath('./span/img')
-                        for emoj in theEmoji: 
+                        for emoj in theEmoji:
                             emoji = emoj.get_attribute('src')
                             emoji = re.findall('\w+(?=.png$)', emoji)
                             emoji = f"U000{emoji[0]}"
@@ -226,7 +226,7 @@ if stsLogin == 1:
                     except Exception as Error:
                         # print(Error)
                         pass
-                        
+
                 thePost = ' '.join(thePosts)
 
                 if idPost not in ListPost:
@@ -238,9 +238,9 @@ if stsLogin == 1:
 
                         with open(fileName, 'wb') as handle:
                                 handle.write(imageFile)
-                        
+
                         listUrlMedia.append(fileNames)
-                    
+
                     listUrlMedia = list(set(listUrlMedia))
                     urlMedia = '|'.join([urlMedia for urlMedia in listUrlMedia if urlMedia])
 
@@ -260,7 +260,7 @@ if stsLogin == 1:
 
         else:
             pass
-        
+
 
     print('------------ finish ------------ ')
 
@@ -282,7 +282,7 @@ db.close_connection(connection)
 # header      = ['NICKNAME', 'DATETIME', 'TAGLINE', 'URL_IMAGE']
 # pathCSV   = os.path.join(path, 'FB.csv')
 
-# with open(pathCSV, 'w', newline ='', encoding="utf-8") as f: 
-#     write = csv.writer(f) 
-#     write.writerow(header) 
-#     write.writerows(listData) 
+# with open(pathCSV, 'w', newline ='', encoding="utf-8") as f:
+#     write = csv.writer(f)
+#     write.writerow(header)
+#     write.writerows(listData)
